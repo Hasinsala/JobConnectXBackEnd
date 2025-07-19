aed4110de9e1fb4afc35a72640c2889412021cf8
Hasinsala <hasinsalaugc@gmail.com>
2025-07-19 18:17:47 +0530

    controller layer

---
+++ src/main/java/com/telusko/joblisting/PostRepository.java
@@ -0,0 +1,5 @@
+package com.telusko.joblisting;
+
+public interface PostRepository {
+
+}
---
+++ src/main/java/com/telusko/joblisting/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.telusko.joblisting.controller;
+
+import com.telusko.joblisting.model.Post;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+
+public class PostController {
+
+
+    @ApiIgnore
+    @RequestMapping(value="/")
+    public void redirect(HttpServletResponse response) throws IOException {
+
+        response.sendRedirect("/swagger-ui.html");
+    }
+
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts(){
+
+        return
+    }
+
+}
